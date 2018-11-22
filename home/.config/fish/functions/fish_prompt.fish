cc49ab774fe898a35b8862d5f492d5e445f2717f
Robert Bus <bus.robert@sonrisa.hu>
2018-11-22 15:06:26 +0100

    Fix git prompt to not use oh-my-fish lib functions

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,46 +1,39 @@
 function fish_prompt
   set -l last_command_status $status
-  set -l cwd (prompt_pwd)
-  set -l host (prompt_hostname)
-  set -l user (whoami)
-  set -l status_ok '^.^'
-  set -l status_error 'O.o'
 
-  set -l ahead    '↑'
-  set -l behind   '↓'
-  set -l diverged '⥄'
-  set -l dirty    '⨯'
-  set -l none     '◦'
-
-  set -l color_ok     (set_color green)
-  set -l color_error  (set_color red)
-  set -l color_normal (set_color normal)
-  set -l color_user   (set_color brred)
-  set -l color_host   (set_color yellow)
-  set -l color_cwd    (set_color green)
-  set -l color_git    (set_color blue)
-
-  echo ''
+  printf '\n'
 
   if test $last_command_status -eq 0
-    echo -n -s '[' $color_ok $status_ok $color_normal ']'
+    printf '[%s^.^%s]' (set_color brgreen) (set_color normal)
   else
-    echo -n -s '[' $color_error $status_error $color_normal ']'
+    printf '[%sO.o%s]' (set_color brred) (set_color normal)
   end
 
-  echo -n -s ' ' $color_user $user $color_normal
-  echo -n -s ' at ' $color_host $host $color_normal
-  echo -n -s ' in ' $color_cwd $cwd $color_normal
+  printf ' %s%s%s' (set_color brcyan) (whoami) (set_color normal)
+  printf ' at %s%s%s' (set_color bryellow) (prompt_hostname) (set_color normal)
+  printf ' in %s%s%s' (set_color brgreen) (prompt_pwd) (set_color normal)
 
-  if git_is_repo
-    echo -n -s ' on ' $color_git (git_branch_name) $color_normal ' '
+  set -g __fish_git_prompt_show_informative_status 'yes'
 
-    if git_is_touched
-      echo -n -s $dirty
-    else
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
-    end
+  set -g __fish_git_prompt_char_stateseparator '|'
+  set -g __fish_git_prompt_char_cleanstate ''
+  set -g __fish_git_prompt_char_dirtystate '!'
+  set -g __fish_git_prompt_char_invalidstate '#'
+  set -g __fish_git_prompt_char_stagedstate '+'
+  set -g __fish_git_prompt_char_stashstate '$'
+  set -g __fish_git_prompt_char_untrackedfiles '?'
+  set -g __fish_git_prompt_char_upstream_ahead '>' 
+  set -g __fish_git_prompt_char_upstream_behind '<'
+  set -g __fish_git_prompt_char_upstream_diverged '<>'
+  set -g __fish_git_prompt_char_upstream_equal '='
+  set -g __fish_git_prompt_char_upstream_prefix ''
+
+  set -l git_status (__fish_git_prompt)
+
+  if [ "$git_status" != "" ]
+    printf ' on%s%s%s' (set_color brwhite) $git_status (set_color normal) 
   end
 
-  echo -e "\n\$ "
+  printf '\n$ '
 end
+
