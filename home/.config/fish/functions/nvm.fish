f23200a49c799c6e15b127fcacb5509a1926abc0
Robert Bus <bus.robert@sonrisa.hu>
2019-01-03 15:07:21 +0100

    Fix NVM wrapper

--- home/.config/fish/functions/nvm.fish
+++ home/.config/fish/functions/nvm.fish
@@ -1,4 +1,4 @@
 # https://github.com/creationix/nvm/issues/303#issuecomment-121086278
 function nvm
-  bass source ~/.nvm/nvm.sh ';' nvm $argv
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
